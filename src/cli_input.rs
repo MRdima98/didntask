438030439d5b2d04a25599e1db0a5a97ffeb14df
MRdima98 <dimafrunza69@gmail.com>
2024-11-24 16:33:13 +0100

    Made nice errors

--- src/cli_input.rs
+++ src/cli_input.rs
@@ -5,12 +5,38 @@ use std::usize;
 
 pub const NO_ARGS: usize = 2;
 
-#[derive(Debug)]
 enum InputError {
+    NoArgs,
     NoFile,
 }
 
 impl Error for InputError {}
+
+impl fmt::Debug for InputError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            InputError::NoArgs => {
+                write!(
+                    f,
+                    "\n\tYOU SHALL PASS AN ARG!\n\
+                \tIt shall be a directory or file?\n\
+                \tConsider passing the whole current directory: didntask .\n\n\
+                \tHere are the options:\n\
+                \t\t --write => This option writes the modified lines to the file"
+                )
+            }
+            InputError::NoFile => {
+                write!(
+                    f,
+                    "\n\tWHAT GAMES ARE YOU PLAYING? THERE IS NO SUCH FILE!\n\
+                    \tCheck out the path and try again!\n\n\
+                    \tHere are the options:\n\
+                    \t\t --write => This option writes the modified lines to the file"
+                )
+            }
+        }
+    }
+}
 
 impl fmt::Display for InputError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -20,23 +46,10 @@ impl fmt::Display for InputError {
 
 pub fn parse_input(args: Vec<String>) -> Result<(), Box<dyn Error>> {
     if args.len() < NO_ARGS {
-        println!(
-            "\nYOU SHALL PASS AN ARG!\n\
-                It shall be a directory or file?\n\
-                Consider passing the whole current directory: didntask .\n\n\
-                Here are the options:\n\
-                \t --write => This option writes the modified lines to the file"
-        );
-        return Err(Box::new(InputError::NoFile));
+        return Err(Box::new(InputError::NoArgs));
     }
 
     if !exists(args[1].clone())? {
-        println!(
-            "\nWHAT GAMES ARE YOU PLAYING? THERE IS NO SUCH FILE!\n\
-                Check out the path and try again!\n\n\
-                Here are the options:\n\
-                \t --write => This option writes the modified lines to the file"
-        );
         return Err(Box::new(InputError::NoFile));
     };
     Ok(())
